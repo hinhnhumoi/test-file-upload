e03e2746e9f37fecdf11f5815f8b66d5915a7026
ext_daothanh <ext_daothanh@asseco-ce.com>
2025-10-28 14:00:29 +0700

    add api to readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-a
+curl --location 'http://localhost:8080/upload/s3' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Basic YXRtTXNVc2VyOm0zYnJWSHlQ' \
+--form 'file=@"/C:/Users/ext_daothanh/Downloads/DOKY description.rtf"'
+
+
+
+curl --location 'http://localhost:8080/upload/local' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Basic YXRtTXNVc2VyOm0zYnJWSHlQ' \
+--form 'file=@"/C:/Users/ext_daothanh/Downloads/Biznis detail.docx"'
--- src/main/java/com/hinhnhumoi/filedemo/FileDemoApplication.java
+++ src/main/java/com/hinhnhumoi/filedemo/FileDemoApplication.java
@@ -1,26 +1,12 @@
 package com.hinhnhumoi.filedemo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
 public class FileDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FileDemoApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner init(StorageProperties properties) {
-        return (args) -> {
-            Files.createDirectories(Paths.get(properties.getLocation()));
-        };
     }
 }
